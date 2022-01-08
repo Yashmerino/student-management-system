8d1aa890a147a6bc5d2888370c25cdc0134af5f6
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-01-08 18:58:24 +0200

    Added window

---
+++ src/main/java/sms/Application.java
@@ -0,0 +1,44 @@
+package sms;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Application {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Application window = new Application();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Application() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Student Management System");
+	}
+
+}
