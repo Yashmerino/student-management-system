7931de5006356c64be709cdae31c85e0960e85da
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-01-12 18:55:45 +0200

    Added commentaries

--- src/main/java/sms/Application.java
+++ src/main/java/sms/Application.java
@@ -1,23 +1,36 @@
 package sms;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import javax.swing.JLabel;
+import java.awt.EventQueue;
 import java.awt.Font;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * The class that manages the front-end part of the application and holds the
+ * actions performed out there
+ * 
+ * @author Artiom
+ *
+ */
 public class Application {
 
+	/**
+	 * The contents of the application
+	 */
 	private JFrame frame;
+	/**
+	 * The text field that stores the login the user has written
+	 */
 	private JTextField loginField;
+	/**
+	 * The text field that stores the password the user has written
+	 */
 	private JTextField passwordField;
 
 	/**
@@ -51,68 +64,51 @@ public class Application {
 		frame.setBounds(100, 100, 640, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Student Management System");
-		
+
+		// The blue-colored panel in the top part of the application
 		JPanel topPanel = new JPanel();
-		topPanel.setBackground(new Color(0, 153, 255));
+		topPanel.setBackground(new Color(0, 155, 255));
 		frame.getContentPane().add(topPanel, BorderLayout.NORTH);
-		
+
+		// The text that informs the user that they have to connect to a database
 		JLabel connectText = new JLabel("Connect to a database:");
 		connectText.setForeground(new Color(255, 255, 255));
 		connectText.setFont(new Font("Tahoma", Font.PLAIN, 50));
 		topPanel.add(connectText);
-		
+
+		// The panel in the bottom part of the application
 		JPanel bottomPanel = new JPanel();
 		frame.getContentPane().add(bottomPanel, BorderLayout.CENTER);
-		
+
+		// The text that informs the user where they have to type the login
 		JLabel loginText = new JLabel("Login:");
+		loginText.setBounds(95, 135, 55, 25);
 		loginText.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
+
+		// The text that informs the user where they have to type the password
 		JLabel passwordText = new JLabel("Password:");
+		passwordText.setBounds(95, 175, 100, 25);
 		passwordText.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
+
+		// Initializes the text field where user writes the login
 		loginField = new JTextField();
+		loginField.setBounds(200, 140, 330, 20);
 		loginField.setColumns(10);
-		
+
+		// Initializes the text field where user writes the password
 		passwordField = new JTextField();
+		passwordField.setBounds(200, 180, 330, 20);
 		passwordField.setColumns(10);
-		
+
+		// The button to press after the login and password were written
 		JButton connectButton = new JButton("Connect");
+		connectButton.setBounds(243, 288, 138, 33);
 		connectButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		GroupLayout gl_bottomPanel = new GroupLayout(bottomPanel);
-		gl_bottomPanel.setHorizontalGroup(
-			gl_bottomPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_bottomPanel.createSequentialGroup()
-					.addGroup(gl_bottomPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_bottomPanel.createSequentialGroup()
-							.addGap(93)
-							.addGroup(gl_bottomPanel.createParallelGroup(Alignment.LEADING)
-								.addComponent(passwordText, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
-								.addComponent(loginText))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_bottomPanel.createParallelGroup(Alignment.LEADING)
-								.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 331, GroupLayout.PREFERRED_SIZE)
-								.addComponent(loginField, GroupLayout.PREFERRED_SIZE, 331, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_bottomPanel.createSequentialGroup()
-							.addGap(243)
-							.addComponent(connectButton, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(97, Short.MAX_VALUE))
-		);
-		gl_bottomPanel.setVerticalGroup(
-			gl_bottomPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_bottomPanel.createSequentialGroup()
-					.addGap(134)
-					.addGroup(gl_bottomPanel.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_bottomPanel.createSequentialGroup()
-							.addGroup(gl_bottomPanel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(loginField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(loginText))
-							.addGap(18)
-							.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addComponent(passwordText, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
-					.addComponent(connectButton, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
-					.addGap(51))
-		);
-		bottomPanel.setLayout(gl_bottomPanel);
+		bottomPanel.setLayout(null);
+		bottomPanel.add(passwordText);
+		bottomPanel.add(loginText);
+		bottomPanel.add(passwordField);
+		bottomPanel.add(loginField);
+		bottomPanel.add(connectButton);
 	}
 }
--- src/main/java/sms/Student.java
+++ src/main/java/sms/Student.java
@@ -1,96 +1,131 @@
 package sms;
 
+/**
+ * The class that holds and manages the information about students
+ * 
+ * @author Artiom
+ *
+ */
 public class Student {
+	/**
+	 * The name of the student
+	 */
 	private String name;
+	/**
+	 * The surname of the student
+	 */
 	private String surname;
+	/**
+	 * The age of the student
+	 */
 	private short age;
+	/**
+	 * The course that the student attends
+	 */
 	private String course;
+	/**
+	 * The year when the student started the course
+	 */
 	private short gradeYear;
+	/**
+	 * The unique id of the student
+	 */
 	int id;
+	/**
+	 * The counter that is increased by one everytime a new instance is created,
+	 * used for ID system
+	 */
 	static int counter;
-	
+
+	/**
+	 * Counter is set to 0 initially
+	 */
 	static {
 		counter = 0;
 	}
 
+	/**
+	 * Default constructor
+	 */
 	Student() {
+		// The unique id of the student is set to value of the counter
 		this.id = counter;
+		// The counter after that is increased by one
 		counter++;
 	}
-	
+
 	/**
-	 * @return the name
+	 * @return The name of the student
 	 */
 	public String getName() {
 		return name;
 	}
 
 	/**
-	 * @param the name to set
+	 * @param The name to set to the student
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	/**
-	 * @return the surname
+	 * @return The surname of the student
 	 */
 	public String getSurname() {
 		return surname;
 	}
 
 	/**
-	 * @param The surname to set
+	 * @param The surname to set to the student
 	 */
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
 
 	/**
-	 * @return the age
+	 * @return The age of the student
 	 */
 	public short getAge() {
 		return age;
 	}
 
 	/**
-	 * @param The age to set
+	 * @param The age to set to the student
 	 */
 	public void setAge(short age) {
 		this.age = age;
 	}
 
 	/**
-	 * @return the course
+	 * @return The course that the student attends
 	 */
 	public String getCourse() {
 		return course;
 	}
 
 	/**
-	 * @param The course to set
+	 * @param The course to set to the student
 	 */
 	public void setCourse(String course) {
 		this.course = course;
 	}
 
 	/**
-	 * @return the id
+	 * @return The id of the student
 	 */
 	public int getId() {
 		return id;
 	}
 
-
 	/**
-	 * @return the gradeYear
+	 * @return The year when student started the course
 	 */
 	public short getGradeYear() {
 		return gradeYear;
 	}
 
 	/**
-	 * @param The gradeYear to set
+	 * @param The year when student started attending the course
 	 */
 	public void setGradeYear(short gradeYear) {
 		this.gradeYear = gradeYear;
