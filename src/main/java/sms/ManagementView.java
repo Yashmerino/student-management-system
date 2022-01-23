e301d9b0fcebcac2cf20079dbfa1d61bbdf97508
Yashmerino <57958281+Yashmerino@users.noreply.github.com>
2022-01-23 12:20:20 +0200

    Restructured the code a bit. No more pop-up error windows when no name/duration and so on have been written

--- src/main/java/sms/ManagementView.java
+++ src/main/java/sms/ManagementView.java
@@ -101,6 +101,9 @@ public class ManagementView {
 		// Clear the selection in the table, to avoid issues with updateDatabase method
 		// when cells are selected
 		table.clearSelection();
+		// Make it visible in constructor, in order to make tests in
+		// ManagementViewTest.java work
+		managementFrame.setVisible(true);
 		DBHandler.updateStudents();
 	}
 
@@ -401,14 +404,12 @@ public class ManagementView {
 		// Actions to perform when "add faculty" button clicked
 		addFacultyButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String facultyName = "";
+				String facultyName;
 
 				facultyName = JOptionPane.showInputDialog(managementFrame, "Type the name of the faculty");
 
-				if (facultyName == null || facultyName.equals("")) {
-					JOptionPane.showMessageDialog(managementFrame,
-							"The faculty hasn't been added!\nPlease type a name for it!", "Error",
-							JOptionPane.ERROR_MESSAGE);
+				if (facultyName == null) {
+					return;
 				} else {
 					if (DBHandler.checkIfElementExists(DBHandler.getFacultiesTable(), facultyName)) {
 						JOptionPane.showMessageDialog(managementFrame,
@@ -445,16 +446,13 @@ public class ManagementView {
 					return;
 				}
 
-				String courseName = "", faculty = "";
+				String courseName, faculty;
 				int duration = 0;
 
 				courseName = JOptionPane.showInputDialog(managementFrame, "Type the name of the course");
 
 				// If no name has been written for the course
-				if (courseName == null || courseName.equals("")) {
-					JOptionPane.showMessageDialog(managementFrame,
-							"The course hasn't been added!\nPlease type a name for it!", "Error",
-							JOptionPane.ERROR_MESSAGE);
+				if (courseName == null) {
 					return;
 				} else {
 					String[] faculties = DBHandler.getFaculties();
@@ -463,16 +461,18 @@ public class ManagementView {
 							faculties[0]);
 
 					// If no faculty has been selected for the course
-					if (faculty == null || faculty.equals("")) {
-						JOptionPane.showMessageDialog(managementFrame,
-								"The course hasn't been added!\nPlease select a faculty for it or add a new one!",
-								"Error", JOptionPane.ERROR_MESSAGE);
+					if (faculty == null) {
 						return;
 					} else {
 						// In case the user types letters for the duration
 						try {
-							duration = Integer.parseInt(JOptionPane.showInputDialog(managementFrame,
-									"Type the duration of the course(months)"));
+							final String durationString = JOptionPane.showInputDialog(managementFrame,
+									"Type the duration of the course(months)");
+
+							if (durationString == null)
+								return;
+
+							duration = Integer.parseInt(durationString);
 						} catch (NumberFormatException ex) {
 							ex.printStackTrace();
 
